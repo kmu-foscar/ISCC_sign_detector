1667b8964c8b3517cedf1f1d8b95b3d4facba085
khc196 <khc146@gmail.com>
2018-04-05 23:25:16 +0900

    Adding Color_Detector and main script

--- Color_Detector.py
+++ Color_Detector.py
@@ -1,22 +1,20 @@
 import cv2
 import numpy as np
-from subprocess import Popen, PIPE
-import os
 
 def nothing(x) :
     pass
 def operate(cam) :
     ret_val, img = cam.read()
-    roi = img[0:240, 320:640]
+    roi = img[0:240, 400:640]
     hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
     lower_red_1 = np.array([0, 50, 0])
     upper_red_1 = np.array([3, 160, 255])
-    lower_red_2 = np.array([160, 50, 0])
+    lower_red_2 = np.array([160, 50, 75])
     upper_red_2 = np.array([180, 160, 255])
     lower_yellow = np.array([10, 100, 0])
     upper_yellow = np.array([25, 180, 255])
     lower_blue = np.array([100, 56, 0])
-    upper_blue = np.array([110, 250, 255]) 
+    upper_blue = np.array([120, 250, 255]) 
 
     mask_red_1 = cv2.inRange(hsv, lower_red_1, upper_red_1)
     mask_red_2 = cv2.inRange(hsv, lower_red_2, upper_red_2)
@@ -29,14 +27,14 @@ def operate(cam) :
     res_blue = cv2.bitwise_and(roi, roi, mask=mask_blue)
     res = cv2.bitwise_or(res_red, res_yellow)
     cv2.bitwise_or(res, res_blue, res)
-    res2 = cv2.medianBlur(res, 15)
 
-    gray = cv2.cvtColor(res2, cv2.COLOR_HSV2BGR)
+    gray = cv2.cvtColor(res, cv2.COLOR_HSV2BGR)
     gray = cv2.cvtColor(gray, cv2.COLOR_BGR2GRAY)
     ret, thresh = cv2.threshold(gray ,5,255, cv2.THRESH_BINARY)
     array_sum = thresh.sum()
-    if(array_sum > 4000000) :
+    cv2.imshow('img', roi)
+    if(array_sum > 3000000) :
         print('Sign Detect!')
         return True, gray
     return False, None
-    cv2.destroyAllWindows()
+    
--- main.py
+++ main.py
@@ -1,12 +1,38 @@
 import Color_Detector 
 import cv2
+import numpy as np
+import tensorflow as tf
+from numpy import newaxis
+from Mininet import * 
+import csv
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 1))
+y = tf.placeholder(tf.int64, (None))
+logits = MiniNet(x)
+inference_operation = tf.argmax(logits, 1)
+saver = tf.train.Saver()
 
 def main():
     cam = cv2.VideoCapture(1)
-    while True:
-        ret, img = Color_Detector.operate()
-        if cv2.waitKey(1) == 27: 
-            break  # esc to quit
+    with tf.Session() as sess:
+        saver.restore(sess, tf.train.latest_checkpoint('.'))
         
+        while True:
+            ret, img = Color_Detector.operate(cam)
+            if ret :
+                img = cv2.resize(img, (32, 32), interpolation=cv2.INTER_AREA)
+                images = np.array([img])
+                images_abs = np.array([cv2.convertScaleAbs(image) for image in images])
+                images_hst_eq = np.array([cv2.equalizeHist(image) for image in images_abs])
+                images_reshaped = images_hst_eq[..., newaxis]
+                images_normalized = images_reshaped - np.mean(images_reshaped)
+                inference_output = sess.run(inference_operation, feed_dict={x: images_normalized})
+                print("Inferred classes:", inference_output)
+                l = sess.run(logits, feed_dict={x: images_normalized})
+                print("logits: ", l)
+
+            if cv2.waitKey(1) == 27: 
+                break  # esc to quit
+        cv2.destroyAllWindows()
 if __name__ == '__main__':
     main()
